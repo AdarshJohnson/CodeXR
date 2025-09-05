df55ababc070354b14ef46da4f11e3ce1aa09686
AdarshJohnson <theadarshjohnson@gmail.com>
2025-09-05 19:14:24 +0530

    test_api.py

--- phase2-debug-rag/test_api.py
+++ phase2-debug-rag/test_api.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 # Replace 'YOUR_API_KEY' with your actual API key
 # It is recommended to use environment variables for security in production
 # genai.configure(api_key=os.environ["API_KEY"])
-genai.configure(api_key='')
+genai.configure(api_key='GEMINI_API_KEY')
 
 # Initialize the model
 model = genai.GenerativeModel("gemini-2.5-flash")
